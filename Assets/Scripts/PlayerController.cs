2de6ee34a3268c5ec6de0e9719b57792e2d381c9
Florent Fortat <florent.fortat@maxgun.fr>
2016-04-17 00:51:07 +0200

    Added control Mecha 3D model with rig/skin

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+    private Rigidbody rigidbody;
 
 	// Use this for initialization
 	void Start () {
-	
+        rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -15,7 +16,9 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate() {
         float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Vertical");
+
+        rigidbody.velocity = new Vector3(x * 50, 0, z * 50);
 
 
     }
